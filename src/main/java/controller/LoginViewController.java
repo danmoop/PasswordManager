bcaa41bec8c9ed5eb131fe988d6a29467d959217
Daniil Durnev <dandurnev1@gmail.com>
2022-04-22 20:28:12 -0700

    reset pass view

--- src/main/java/controller/LoginViewController.java
+++ src/main/java/controller/LoginViewController.java
@@ -1,28 +1,15 @@
 package controller;
 
-import com.cs151.team2.application.Main;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import tools.SceneManager;
 
 public class LoginViewController {
 
     public void openSignUpPage(ActionEvent event) {
-        switchToView(event, "signup-view.fxml");
+        SceneManager.switchToView(event, "signup-view.fxml", 900, 600);
     }
 
-    private void switchToView(ActionEvent event, String viewName) {
-        try {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(viewName));
-            Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-            stage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void openResetPassPage() {
+        SceneManager.openView("reset-pass.fxml", 460, 240);
     }
 }
--- src/main/java/controller/SignUpViewController.java
+++ src/main/java/controller/SignUpViewController.java
@@ -1,27 +1,10 @@
 package controller;
 
-import com.cs151.team2.application.Main;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import tools.SceneManager;
 
 public class SignUpViewController {
     public void openLoginPage(ActionEvent event) {
-        switchToView(event, "login-view.fxml");
-    }
-
-    private void switchToView(ActionEvent event, String viewName) {
-        try {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(viewName));
-            Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-            stage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SceneManager.switchToView(event, "login-view.fxml", 900, 600);
     }
 }
